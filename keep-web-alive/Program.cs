a966596294d0d81235ecc5c1fb6a965be6a3572f
Joseph Choi <jsk.choi@gmail.com>
2018-10-18 10:13:33 -0700

    init

---
+++ keep-web-alive/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Configuration;
+
+namespace keep_web_alive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string urls = ConfigurationManager.AppSettings["urls"];
+
+                foreach (var url in urls.Split(','))
+                {
+                    string htmlCode = client.DownloadString("https://www.google.com");
+                    Console.WriteLine($"did - {url}");
+                }
+            }
+        }
+    }
+}
